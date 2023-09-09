966acd3280b359d313d5899c2eb746beb29257fb
Simone <sisikasilva@hotmail.com>
2023-09-09 10:51:30 -0300

    meu arquivo hello world

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello Word!')
+
+nome = "Simone"
+profissao = "Estudante"
+
+print(f"Este é o primeiro programa de {nome}")
+print("Eu sou", profissao,"e decidi trilhar esse caminho do python")
+
+resposta = input("Você está pronto para embarcar comigo? (S/N)")
