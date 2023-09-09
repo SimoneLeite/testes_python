26674d22a1accc654f68e8ddab56cbf50318c787
Simone <sisikasilva@hotmail.com>
2023-09-09 15:16:56 -0300

    Atividades aritmeticas

---
+++ aritmeticos.py
@@ -0,0 +1,73 @@
+
+print("======Operadores Aritmeticos=====")
+print("=================================")
+
+A = 7
+B = 2
+C = 4
+D = 14
+
+#1. A + B - C;
+
+total = A + B - C
+
+print(f"O tota do item 1 é? {total}")
+
+ #A * B / C
+
+total =  A * B / C
+
+print(f"O total do item  2 é? {total}")
+
+#C + 22 * D;
+
+total = C + 22 * D
+
+print(f"O total do item  3 é? {total}")
+
+#4. A + B * C + D;
+
+total = A + B * C + D
+
+print(f"O total do item  4 é? {total}")
+
+#. (A + B) * (C + D);
+
+total = (A + B) * (C + D)
+
+print(f"O total do item  5 é? {total}")
+
+
+#. (D / A) / (B / C);
+
+total = (D / A) / (B / C)
+
+print(f"O total do item  6 é? {total}")
+
+#  10 ** A;
+
+total = 10 ** A
+
+print(f"O total do item  7 é? {total}")
+
+#8. 58 % A;
+
+total = 58 % A
+
+print(f"O total do item  8 é? {total}")
+
+#9. (A + B + C * D) / (B - C * (B - C))
+
+total = (A + B + C * D) / (B - C * (B - C))
+
+print(f"O total do item  9 é? {total}")
+
+
+#10. (A + B + C * D) / (B - C * (B - C)) + 10 ** A
+
+total = (A + B + C * D) / (B - C * (B - C)) + 10 ** A
+
+print(f"O total do item  10 é? {total}")
+
+
+
