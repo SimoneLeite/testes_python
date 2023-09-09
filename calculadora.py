f86ea756c45fe5f7a67c0c606b2f71f7bfab5645
Simone <sisikasilva@hotmail.com>
2023-09-09 17:32:30 -0300

    Calculadora Notas

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+#Peça ao usuário para inserir dois números e uma operação (+, -, *, /).
+#Realize a operação e exiba o resultado.
+
+print("=======CALCULADORA SIMPLES======")
+print("================================")
+
+numero1 = float(input("Informe um número? "))
+numero2 = float(input("Informe um número? "))
+operacao = input("Escolha umas das operaçoes (+, -, *, /)")
+
+if operacao == "+":
+    resultado = numero1 + numero2
+    print(f"O resultado é : {resultado} ")
+elif operacao == "-":
+    resultado = numero1 - numero2
+    print(f"O resultado da subtração é : {resultado}")
+elif operacao == "*":
+     resultado = numero1 * numero2
+     print(f"O resultado da subtração é :{resultado}")
+elif operacao == "/":
+     resultado = numero1 / numero2
+     print(f"O resultado da divisão é : {resultado}")
+
+    
+    
+
+
---
+++ notas.py
@@ -0,0 +1,20 @@
+#Peça ao usuário para inserir uma nota de 0 a 100 e, em seguida, classifique-a como
+#"A" (90-100), "B" (80-89), "C" (70-79),
+#"D" (60-69) ou "F" (abaixo de 60).
+
+print("====Notas====")
+
+nota = float(input("informe sua nota de 0 a 100 ?"))
+if  nota >= 90 :
+    print(f"Sua classificação é a A") 
+elif nota >= 80 and  nota <= 89:
+    print(f"Sua classificação é a B") 
+elif nota >= 70 and nota <= 79 :
+    print(f"Sua classificação é a C")
+elif nota >= 60 and nota <= 69 :
+    print(f"Sua classificação é a D")   
+elif nota <=60 :
+    print(f"Sua classificção é a F")     
+else :
+    print("Esse item não existe")
+
